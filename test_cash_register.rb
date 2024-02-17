9d9e117df4adb8a42ff9423cb8bc53e7209f6d26
Grzegorz Milanowski <grzegorz.milanowski@zeneducate.com>
2024-02-17 12:27:21 +0100

    Refactor test case to include product amounts and data

--- test_cash_register.rb
+++ test_cash_register.rb
@@ -2,9 +2,15 @@ require 'minitest/autorun'
 require_relative 'cash_register'
 
 describe CashRegister do
-  it 'returns products' do
-    cr = CashRegister.new(['book', 'music cd', 'chocolate bar'])
-    result = cr.products
-    assert_equal result, ['book', 'music cd', 'chocolate bar']
+  it 'returns products with amounts and all the data' do
+    products = [
+      { name: 'book', imported: false, category: 'book', price: 12.49, amount: 2 },
+      { name: 'music cd', imported: false, category: 'other', price: 14.99, amount: 1 },
+      { name: 'chocolate bar', imported: false, category: 'food', price: 0.85, amount: 1 }
+    ]
+    cash_register = CashRegister.new(products)
+    assert_equal cash_register.products products
   end
+
+
 end
